8670223989880ec2ef431cc7af5772bae142628b
hidayatallahaatwit <hidayatallaha@wit.edu>
2023-10-03 14:49:03 -0400

    Favorite day of the week
    Fav day of the week

---
+++ src/edu/wit/cs/comp1050/Q05.java
@@ -0,0 +1,23 @@
+package edu.wit.cs.comp1050;
+import java.util.Scanner;
+
+
+public class Q05 {
+	
+public static void main(String[] args) {
+	
+	Scanner in = new Scanner(System.in);
+	
+	String day;
+	
+	System.out.println("What's your favorite day of the week?");
+	day = in.nextLine();
+	
+	
+	
+}
+
+
+
+
+}
